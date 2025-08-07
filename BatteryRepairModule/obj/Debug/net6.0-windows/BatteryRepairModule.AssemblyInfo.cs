6c5170fa50bf7251a3a11340186c19ef72a4feaa
FMustico <swdev2@twigpwr.com>
2025-08-07 13:36:10 -0400

    Updated Operator Password
    - Updated Database User Operator's Password:
    SERVER PASSWORD => TWIGOperator

--- BatteryRepairModule/obj/Debug/net6.0-windows/BatteryRepairModule.AssemblyInfo.cs
+++ BatteryRepairModule/obj/Debug/net6.0-windows/BatteryRepairModule.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatteryRepairModule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92e6653afce60ad8ae73833c1e6ecfb5edcc3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cb2a58c97cbc52602193e5b9e5b4287cfe8b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatteryRepairModule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatteryRepairModule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BatteryRepairModule/util/databaseConnection.cs
+++ BatteryRepairModule/util/databaseConnection.cs
@@ -6,6 +6,6 @@ namespace BatteryRepairModule;
 
 public static class dbConnection
 {
-    public static string connectionPath = "Server=192.168.1.35;Port=5432;Username=operator;Password=aTWIGsql25;Database=BRM_Room";
+    public static string connectionPath = "Server=192.168.1.35;Port=5432;Username=operator;Password=TWIGOperator;Database=BRM_Room";
     
 }
